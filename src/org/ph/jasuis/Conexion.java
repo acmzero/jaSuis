37c6e7dcad80f53c51dccccf0fb57d314b3aa7c4
Heli <acmzero@gmail.com>
2012-04-28 13:26:22 -0500

    agregada conexion general

---
+++ src/org/ph/jasuis/Conexion.java
@@ -0,0 +1,42 @@
+package org.ph.jasuis;
+
+import java.sql.*;
+
+public class Conexion {
+	private Statement stat; // variable para ejecucion de sql
+	private Connection conn;
+
+	public Conexion() throws Exception {
+		Class.forName("org.sqlite.JDBC");
+		conn = DriverManager.getConnection("jdbc:sqlite:jaSuis.db");
+
+		stat = conn.createStatement();
+		conn.setAutoCommit(true);
+	}
+
+	public ResultSet ejecutarSql(String sql) {
+
+		ResultSet rs = null;
+		try {
+			rs = stat.executeQuery(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return rs;
+
+	}
+
+	public void cerrarConexion() {
+		try {
+			conn.commit();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public Connection getConeccion() {
+		return conn;
+	}
+}
